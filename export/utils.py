a50e9b343956be8f5975ee3578b289562a70ce31
awsh10 <awsh@ukr.net>
2015-11-04 12:24:07 +0200

    remove classes, correct

--- export/utils.py
+++ export/utils.py
@@ -21,54 +21,42 @@ def to_pdf(product_list, template):
         raise exceptions.PDFExportError('Cannot convert product list to PDF: {}'.format(pdf.err))
 
 
-class Common(object):
-    def __init__(self, product_list, template):
-        self.result = StringIO.StringIO()
-        rendered_template = render_to_string(template, {'product_list': product_list})
-        self.doc = html.document_fromstring(rendered_template)
-
-
-class CsvObject(Common):
-    def get(self):
-        writer = csv.writer(self.result)
-
-        for table_row in self.doc.findall('.//tr'):
-            table_row_text = [el.text_content() for el in table_row]
-            writer.writerow(table_row_text)
-
-        return self.result.getvalue()
-
-
-class XlsObject(Common):
-    def get(self):
-        workbook  = xlsxwriter.Workbook(self.result)
-        worksheet = workbook.add_worksheet()
-
-        row_counter = 0
-        for table_row in self.doc.findall('.//tr'):
-
-            column_counter = 0
-            for el in table_row:
-                worksheet.write(row_counter, column_counter, el.text_content() )
-                column_counter += 1
-
-            row_counter += 1
-        format1 = workbook.add_format({'bold': True})
-        format1.set_align('center')
-        format2 = workbook.add_format({'num_format': True})
-        format2.set_align('right')
-        worksheet.set_row(0, 15, format1)
-        worksheet.set_column('B:B', 10, format2)
-        worksheet.set_column('A:A', 20)
-        workbook.close()
-        return self.result.getvalue()
+def _to_string(product_list, template):
+    result = StringIO.StringIO()
+    rendered_template = render_to_string(template, {'product_list': product_list})
+    doc = html.document_fromstring(rendered_template)
+    return [result, doc]
 
 
 def to_csv(product_list, template):
     """ Create CSV file with product list """
-    return CsvObject(product_list, template).get()
+    result, doc = _to_string(product_list, template)
+    writer = csv.writer(result)
+
+    for row_index in doc.findall('.//tr'):
+        row_index_text = [el.text_content() for el in row_index]
+        writer.writerow(row_index_text)
+
+    return result.getvalue()
 
 
 def to_xls(product_list, template):
     """ Create xls file with product list """
-    return XlsObject(product_list, template).get()
+    result, doc = _to_string(product_list, template)
+    workbook  = xlsxwriter.Workbook(result)
+    worksheet = workbook.add_worksheet()
+
+    for row_index, row in enumerate(doc.findall('.//tr')):
+        for column_index, el in enumerate(row):
+            worksheet.write(row_index, column_index, el.text_content())
+
+    header_format = workbook.add_format({'bold': True})
+    header_format.set_align('center')
+    column_format = workbook.add_format({'num_format': True})
+    column_format.set_align('right')
+    worksheet.set_row(0, 15, header_format)
+    worksheet.set_column('B:B', 10, column_format)
+    worksheet.set_column('A:A', 20)
+    workbook.close()
+
+    return result.getvalue()
